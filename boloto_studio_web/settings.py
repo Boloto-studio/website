85f28e5546c2541dc7ae86e0c24314402beefb87
Andrew Dzobko <mytyber@gmail.com>
2025-11-07 19:33:15 +0000

    Refactor database settings to unify configuration for development and production environments

--- boloto_studio_web/settings.py
+++ boloto_studio_web/settings.py
@@ -92,24 +92,16 @@ WSGI_APPLICATION = 'boloto_studio_web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv("PROD_DB_NAME"),
-            'USER': os.getenv("PROD_DB_USER"),
-            'PASSWORD': os.getenv("PROD_DB_PASSWORD"),
-            'HOST': os.getenv("PROD_DB_HOST"),
-            'PORT': os.getenv("PROD_DB_PORT"),
-        }
-    }
+}
 
 
 # Password validation
