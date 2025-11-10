9ae8f32162ce8843553104eb58d555bb82cfed79
Andrew Dzobko <mytyber@gmail.com>
2025-11-10 07:30:06 +0000

    Refactor database settings to support connection string for dynamic configuration

--- boloto_studio_web/settings.py
+++ boloto_studio_web/settings.py
@@ -92,16 +92,23 @@ WSGI_APPLICATION = 'boloto_studio_web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+if os.getenv("DB_STRING"):
+    import dj_database_url
+
+    DATABASES = {
+        'default': dj_database_url.parse(os.getenv("DB_STRING"))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv("DB_NAME"),
+            'USER': os.getenv("DB_USER"),
+            'PASSWORD': os.getenv("DB_PASSWORD"),
+            'HOST': os.getenv("DB_HOST"),
+            'PORT': os.getenv("DB_PORT"),
+        }
+    }
 
 
 # Password validation
